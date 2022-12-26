80de9a649fdc3796456f53ae9a9018d0df57f322
VeronikaSemenkova <nika.siemienkova145@gmail.com>
2022-12-26 16:40:31 +0300

    Initial Commit

---
+++ LoginScreenApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreenApplication
+//
+//  Created by Nika Semenkova on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
